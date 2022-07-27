9c379d5e1137797a9add289b5f0def47596ecec1
Sellular <sellular13@gmail.com>
2022-07-27 04:02:50 +0000

    Welcome mostly working. Using guild config

--- views/WelcomeView.py
+++ views/WelcomeView.py
@@ -1,5 +1,8 @@
+from types import GenericAlias
 import discord
 from discord.ui import View
+
+from utils import GeneralUtils
 
 import asyncio
 
@@ -15,10 +18,16 @@ class WelcomeView(View):
     @discord.ui.button(label="Click here to start!", style=discord.ButtonStyle.green, custom_id="welcome_button")
     async def callback(self, interaction: discord.Interaction, button: discord.Button):
         user = interaction.user
-        role = interaction.guild.get_role(987553778662268928)
+
+        guildConfig = GeneralUtils.getConfig('config.ini', 'guild')
+        newUserRoleId = guildConfig['new_user_role_id']
+
+        role = discord.utils.get(interaction.guild.roles, id = int(newUserRoleId))
         await user.add_roles(role)
         await interaction.response.defer(ephemeral=True, thinking=True)
         await asyncio.sleep(0.5)
-        await interaction.followup.send(f"You now have the onboarding role! Head over to the <#987921414944350269> channel to pick some more!")
+
+        rolesChannelId = guildConfig['roles_channel_id']
+        await interaction.followup.send(f"You now have the onboarding role! Head over to the <#{rolesChannelId}> channel to pick some more!")
 
         self.value=False
