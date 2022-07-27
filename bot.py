d4d721c08a2098092faa09bb15df42a4866c7057
Sellular <sellular13@gmail.com>
2022-07-27 03:30:44 +0000

    Added view setup method

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 
 from utils import SetupUtils, GeneralUtils
 
-from views import WelcomeView, PronounsView, PrononsView, GenreChooseView, GenreChoose2View, GenreChooseV2View, GenreChooseV3View, DoneButtonView, LastMSGV2View, LastMSGView, VerificationView
-
 class MyClient(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
@@ -14,24 +12,13 @@ class MyClient(commands.Bot):
         super().__init__(command_prefix=('!'), intents=intents)
 
     async def setup_hook(self) -> None:
-        self.add_view(WelcomeView())
-        self.add_view(PronounsView())
-        self.add_view(PrononsView())
-        self.add_view(GenreChooseView())
-        self.add_view(GenreChoose2View())
-        self.add_view(GenreChooseV2View())
-        self.add_view(GenreChooseV3View())
-        self.add_view(LastMSGView())
-        self.add_view(LastMSGV2View())
-        self.add_view(DoneButtonView())
-        self.add_view(VerificationView())
-
+        SetupUtils.importViews(self)        
         await SetupUtils.importCogs(self)
 
     async def on_ready(self):
         print("Bot running with:")
-        print("Username: ", bot.user.name)
-        print("User ID: ", bot.user.id)
+        print("Username: ", self.user.name)
+        print("User ID: ", self.user.id)
         print('-----')
 
 bot = MyClient()
--- utils/SetupUtils.py
+++ utils/SetupUtils.py
@@ -1,6 +1,8 @@
 import discord, os
 from discord.ext import commands
 
+from views import WelcomeView, PronounsView, PrononsView, GenreChooseView, GenreChoose2View, GenreChooseV2View, GenreChooseV3View, DoneButtonView, LastMSGV2View, LastMSGView, VerificationView
+
 def setupBot():
     description = 'An administration bot'
 
@@ -10,6 +12,19 @@ def setupBot():
     intents.bans = True
 
     return commands.Bot(command_prefix=('-', '='), intents=intents, description=description)
+
+def importViews(bot: discord.ext.commands.Bot):
+    bot.add_view(WelcomeView())
+    bot.add_view(PronounsView())
+    bot.add_view(PrononsView())
+    bot.add_view(GenreChooseView())
+    bot.add_view(GenreChoose2View())
+    bot.add_view(GenreChooseV2View())
+    bot.add_view(GenreChooseV3View())
+    bot.add_view(LastMSGView())
+    bot.add_view(LastMSGV2View())
+    bot.add_view(DoneButtonView())
+    bot.add_view(VerificationView())
 
 async def importCogs(bot: discord.ext.commands.Bot):
     for filename in os.listdir("./cogs/commands"):
