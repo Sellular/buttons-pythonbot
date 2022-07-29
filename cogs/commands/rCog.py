53ec8ed2f687105a84d0ffbc242e0a9ebaa14e98
Sellular <sellular13@gmail.com>
2022-07-29 02:12:45 +0000

    Using new RoleSelectView and trying to add views differently

--- cogs/commands/rCog.py
+++ cogs/commands/rCog.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 
-from views import PronounsView, GenreChooseView, DoneButtonView, LastMSGView
+from utils import GeneralUtils
+from views import PronounsView, DoneButtonView, RoleChooseView
 
 class rCog(commands.Cog):
     def __init__(self, bot):
@@ -9,10 +10,23 @@ class rCog(commands.Cog):
     
     @commands.command()
     async def r(self, ctx):
-        await ctx.send("Pick your pronoun roles here! We've got 'em all.", view=PronounsView())
-        await ctx.send("Choose your genre preferences! Don't see your favs? You can suggest it in <#962242442193670204>.", view=GenreChooseView())
-        await ctx.send('Do you like making music? Are you interested in things like beat loops and DAWs? Then the __producer__ role is for you! Are you interested in speaker setups? Do you own/want a high quality DAC and some high impedance headphones? Then the __audiophile__ role is for you!', view=LastMSGView())
-        await ctx.send('Once you have finished selecting your roles, click this button to continue your onboarding', view=DoneButtonView())
+        genreArray = GeneralUtils.getMusicGenres()
+        hobbyArray = GeneralUtils.getHobbies()
+
+        pronounsView = PronounsView()
+        genreChooseView = RoleChooseView(options = genreArray, custom_id = "genre_select")
+        hobbyChooseView = RoleChooseView(options = hobbyArray, custom_id = "hobby_select")
+        doneButtonView = DoneButtonView()
+
+        await ctx.send("Pick your pronoun roles here! We've got 'em all.", view=pronounsView)
+        await ctx.send("Choose your genre preferences! Don't see your favs? You can suggest it in <#962242442193670204>.", view=genreChooseView)
+        await ctx.send('Do you like making music? Are you interested in things like beat loops and DAWs? Then the __producer__ role is for you! Are you interested in speaker setups? Do you own/want a high quality DAC and some high impedance headphones? Then the __audiophile__ role is for you!', view=hobbyChooseView)
+        await ctx.send('Once you have finished selecting your roles, click this button to continue your onboarding', view=doneButtonView)
+
+        self.bot.add_view(pronounsView)
+        self.bot.add_view(genreChooseView)
+        self.bot.add_view(hobbyChooseView)
+        self.bot.add_view(doneButtonView)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(rCog(bot))
--- cogs/commands/sCog.py
+++ cogs/commands/sCog.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 
-from views import PrononsView, GenreChooseView, LastMSGV2View
+from utils import GeneralUtils
+from views import PrononsView, RoleChooseView
 
 class sCog(commands.Cog):
     def __init__(self, bot):
@@ -9,9 +10,20 @@ class sCog(commands.Cog):
     
     @commands.command() 
     async def s(self, ctx): 
-        await ctx.send("Pick your pronoun roles here! We've got 'em all.", view=PrononsView())
-        await ctx.send("Choose your genre preferences! Don't see your favs? You can suggest it in <#962242442193670204>.", view=GenreChooseView(removeIfExist=True))
-        await ctx.send('Do you like making music? Are you interested in things like beat loops and DAWs? Then the __producer__ role is for you! Are you interested in speaker setups? Do you own/want a high quality DAC and some high impedance headphones? Then the __audiophile__ role is for you!', view=LastMSGV2View())
+        genreArray = GeneralUtils.getMusicGenres()
+        hobbyArray = GeneralUtils.getHobbies()
+
+        pronounsView = PrononsView()
+        genreChooseView = RoleChooseView(options = genreArray, custom_id = "update_genre_select", removeIfExist=True)
+        hobbyChooseView = RoleChooseView(options = hobbyArray, custom_id = 'update_hobby_select', removeIfExist=True)
+
+        await ctx.send("Pick your pronoun roles here! We've got 'em all.", view=pronounsView)
+        await ctx.send("Choose your genre preferences! Don't see your favs? You can suggest it in <#962242442193670204>.", view=genreChooseView)
+        await ctx.send('Do you like making music? Are you interested in things like beat loops and DAWs? Then the __producer__ role is for you! Are you interested in speaker setups? Do you own/want a high quality DAC and some high impedance headphones? Then the __audiophile__ role is for you!', view=hobbyChooseView)
+
+        self.bot.add_view(pronounsView)
+        self.bot.add_view(genreChooseView)
+        self.bot.add_view(hobbyChooseView)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(sCog(bot))
