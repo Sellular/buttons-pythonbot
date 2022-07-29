3dc3123ced387e9e9e01d8594b0a46d3649aeb7a
Sellular <sellular13@gmail.com>
2022-07-29 02:11:05 +0000

    Removed initialization of views. For now

--- utils/SetupUtils.py
+++ utils/SetupUtils.py
@@ -1,17 +1,4 @@
 import discord, os
-from discord.ext import commands
-
-from views import WelcomeView, PronounsView, PrononsView, GenreChooseView, DoneButtonView, LastMSGV2View, LastMSGView, VerificationView
-
-def importViews(bot: discord.ext.commands.Bot):
-    bot.add_view(WelcomeView())
-    bot.add_view(PronounsView())
-    bot.add_view(PrononsView())
-    bot.add_view(GenreChooseView())
-    bot.add_view(LastMSGView())
-    bot.add_view(LastMSGV2View())
-    bot.add_view(DoneButtonView())
-    bot.add_view(VerificationView())
 
 async def importCogs(bot: discord.ext.commands.Bot):
     for filename in os.listdir("./cogs/commands"):
