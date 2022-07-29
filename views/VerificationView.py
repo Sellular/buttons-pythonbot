35af28aeffeaf6832e891d8602015b68585e93fe
Sellular <sellular13@gmail.com>
2022-07-29 16:15:59 -0500

    Usable roles

--- views/VerificationView.py
+++ views/VerificationView.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ui import View
 
+from utils import GeneralUtils
+
 import asyncio
 
 class VerificationView(View):
@@ -9,9 +11,14 @@ class VerificationView(View):
     @discord.ui.button(label="Access the server!", style = discord.ButtonStyle.green, custom_id='verify_button')
     async def on_callback(self, interaction: discord.Interaction, button: discord.Button):
         bot = interaction.client
-        guild = bot.get_guild(886770756262961172)
+        guild = interaction.guild
+        guildConfig = GeneralUtils.getConfig('guild')
         member = interaction.user
-        member_role = guild.get_role(886804525548187708)
-        await interaction.response.defer(ephemeral=True, thinking=True)
-        await asyncio.sleep(0.5)
-        await interaction.followup.send("You now have the blues role! Enjoy your stay in our server!!")
+        member_role = discord.utils.get(guild.roles, id = int(guildConfig['new_member_role_id']))
+        if member_role:
+            await member.add_roles(member_role)
+            await interaction.response.defer(ephemeral=True, thinking=True)
+            await asyncio.sleep(0.5)
+            await interaction.followup.send("You now have the new member role! Enjoy your stay in our server!!")
+        else:
+            await interaction.response.send_message("Role not found. Contact bot developer or server admin")
