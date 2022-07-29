2bf46fb851b799dbb943bc4a7dc461ead611b6ab
Sellular <sellular13@gmail.com>
2022-07-29 16:16:18 -0500

    Implement updateMode

--- views/components/RoleSelect.py
+++ views/components/RoleSelect.py
@@ -2,26 +2,28 @@ import discord
 from discord.ui import Select
 
 class RoleSelect(Select):
-    removeIfExist = False
+    updateMode = False
 
-    def __init__(self, placeholder: str, custom_id: str, removeIfExist: bool = False):
-        self.removeIfExist = removeIfExist
+    def __init__(self, placeholder: str, custom_id: str, updateMode: bool = False):
+        self.updateMode = updateMode
         super().__init__(placeholder = placeholder, custom_id = custom_id)
     
     async def callback(self, interaction: discord.Interaction):
-        guild = interaction.guild
-        member = interaction.user
         selectedRoleID = int(self.values[0])
-
-        await interaction.response.edit_message(view = self.view)
+        guild = interaction.guild
         role = discord.utils.get(guild.roles, id = selectedRoleID)
+        if self.updateMode:
+            member = interaction.user
+            
+            await interaction.response.edit_message(view = self.view)
+            
+            followupPrefix = "Assigned"
+            if member.get_role(role.id):
+                await member.remove_roles(role)
+                followupPrefix = "Removed"
+            else:
+                await member.add_roles(role)
 
-        followupPrefix = "Assigned"
-        if self.removeIfExist and member.get_role(role.id):
-            await member.remove_roles(role)
-            followupPrefix = "Removed"
+            await interaction.followup.send(f"{followupPrefix} role **{role.name}**.", ephemeral = True)
         else:
-            await member.add_roles(role)
-
-        await interaction.followup.send(f"{followupPrefix} role **{role.name}**.", ephemeral = True)
-        
+            await interaction.response.send_message(f"Your roles have been saved, and you'll receive them when you finish onboarding!", ephemeral = True)
