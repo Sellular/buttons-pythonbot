b168894668b7753ff336e894816f97e4c9d8aede
Sellular <sellular13@gmail.com>
2022-08-01 00:34:02 +0000

    remove waits

--- views/VerificationView.py
+++ views/VerificationView.py
@@ -3,22 +3,17 @@ from discord.ui import View
 
 from utils import GeneralUtils
 
-import asyncio
-
 class VerificationView(View):
     def __init__(self):
         super().__init__(timeout=None)
     @discord.ui.button(label="Access the server!", style = discord.ButtonStyle.green, custom_id='verify_button')
     async def on_callback(self, interaction: discord.Interaction, button: discord.Button):
-        bot = interaction.client
         guild = interaction.guild
         guildConfig = GeneralUtils.getConfig('guild')
         member = interaction.user
-        member_role = discord.utils.get(guild.roles, id = int(guildConfig['new_member_role_id']))
+        member_role = discord.utils.get(guild.roles, id = int(guildConfig['member_role_id']))
         if member_role:
             await member.add_roles(member_role)
-            await interaction.response.defer(ephemeral=True, thinking=True)
-            await asyncio.sleep(0.5)
-            await interaction.followup.send("You now have the new member role! Enjoy your stay in our server!!")
+            await interaction.response.send_message("You now have the new member role! Enjoy your stay in our server!!")
         else:
             await interaction.response.send_message("Role not found. Contact bot developer or server admin")
--- views/WelcomeView.py
+++ views/WelcomeView.py
@@ -3,24 +3,15 @@ from discord.ui import View
 
 from utils import GeneralUtils
 
-import asyncio
 from datetime import datetime, timedelta, timezone
 
 class WelcomeView(View):
     def __init__(self):
         super().__init__(timeout=None)
-    # async def interaction_check(self, interaction):
-    #     user = interaction.user
-    #     if user.joined_at(second=15):
-    #         await interaction.response.send_message("You haven't read the rules yet.", ephemeral=True)    
-    #         return False
-    #     return True
     @discord.ui.button(label="Click here to start!", style=discord.ButtonStyle.green, custom_id="welcome_button")
     async def callback(self, interaction: discord.Interaction, button: discord.Button):
         user = interaction.user
         now = datetime.now(timezone.utc)
-
-        await interaction.response.defer(ephemeral=True, thinking=True)
 
         if abs(now - user.joined_at) < timedelta(seconds = 30):
             await interaction.followup.send("Thanks for being excited to join our community! Please take a moment to read the above messages and try clicking this button again in a bit.")
@@ -31,7 +22,6 @@ class WelcomeView(View):
 
         role = discord.utils.get(interaction.guild.roles, id = int(onboardingRoleId))
         await user.add_roles(role)
-        # await asyncio.sleep(0.5)
 
         rolesChannelId = guildConfig['add_roles_channel_id']
         await interaction.followup.send(f"You now have the onboarding role! Head over to the <#{rolesChannelId}> channel to pick some more!")
