291f6d8b91d3537e60ea1b4678a3ebb70a4e5e34
Sellular <sellular13@gmail.com>
2022-08-04 21:31:22 +0000

    Moved notification role choose

--- utils/DiscordUtils.py
+++ utils/DiscordUtils.py
@@ -27,22 +27,22 @@ async def sendRoles(bot: discord.Client, updateMode: bool):
         int(guildConfig[f'{"update" if updateMode else "add"}_roles_channel_id']))
     pronounArray = GeneralUtils.getPronouns()
     genreArray = GeneralUtils.getMusicGenres()
-    notificationArray = GeneralUtils.getNotifications()
     hobbyArray = GeneralUtils.getHobbies()
-
+    notificationArray = GeneralUtils.getNotifications()
+    
     pronounChooseView = RoleChooseView(
         options=pronounArray, custom_id=f"{'update_' if updateMode else ''}pronoun_select", updateMode=updateMode)
     genreChooseView = RoleChooseView(
         options=genreArray, custom_id=f"{'update_' if updateMode else ''}genre_select", updateMode=updateMode)
-    notificationChooseView = RoleChooseView(
-        options=notificationArray, custom_id=f"{'update_' if updateMode else ''}notification_select", updateMode=updateMode)
     hobbyChooseView = RoleChooseView(
         options=hobbyArray, custom_id=f"{'update_' if updateMode else ''}hobby_select", updateMode=updateMode)
-
+    notificationChooseView = RoleChooseView(
+        options=notificationArray, custom_id=f"{'update_' if updateMode else ''}notification_select", updateMode=updateMode)
+    
     await channel.send("Pick your pronoun roles here! We've got 'em all.", view=pronounChooseView)
     await channel.send("Choose your genre preferences! Don't see your favs? You can suggest it in <#962242442193670204>.", view=genreChooseView)
-    await channel.send('Want to get notified about the latest events? Want to know about the newest releases in the music world? Notification roles are all here!', view=notificationChooseView)
     await channel.send('Do you like making music? Are you interested in things like beat loops and DAWs? Then the __producer__ role is for you! Are you interested in speaker setups? Do you own/want a high quality DAC and some high impedance headphones? Then the __audiophile__ role is for you!', view=hobbyChooseView)
+    await channel.send('Want to get notified about the latest events? Want to know about the newest releases in the music world? Notification roles are all here!', view=notificationChooseView)
     if not updateMode:
         doneButtonView = RoleSubmitButtonView(select_views=[
                                               pronounChooseView, genreChooseView, notificationChooseView, hobbyChooseView], custom_id="submit_button")
