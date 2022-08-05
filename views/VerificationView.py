6045ce2f3826a0b8073be70217205b0c526927da
Sellular <sellular13@gmail.com>
2022-08-05 13:23:48 +0000

    Added verification button message

--- views/VerificationView.py
+++ views/VerificationView.py
@@ -3,6 +3,7 @@ from discord.ui import View
 
 from utils import GeneralUtils
 
+import asyncio
 
 class VerificationView(View):
     def __init__(self):
@@ -14,17 +15,20 @@ class VerificationView(View):
         guildConfig = GeneralUtils.getConfig('guild')
         member = interaction.user
 
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        await asyncio.sleep(0.2) # Thinking...
+
         onboarding_role = discord.utils.get(
             guild.roles, id=int(guildConfig['onboarding_role_id']))
         if onboarding_role:
             await member.remove_roles(onboarding_role)
         else:
-            await interaction.response.send_message("Role not found. Contact bot developer or server admin")
+            await interaction.followup.send("Role not found. Contact bot developer or server admin")
 
         member_role = discord.utils.get(
             guild.roles, id=int(guildConfig['member_role_id']))
         if member_role:
             await member.add_roles(member_role)
-            await interaction.response.defer(ephemeral=True, thinking=True)
+            await interaction.followup.send(f"You now have the {member_role.name} role! Enjoy your stay in our server!!")
         else:
-            await interaction.response.send_message("Role not found. Contact bot developer or server admin")
+            await interaction.followup.send("Role not found. Contact bot developer or server admin")
