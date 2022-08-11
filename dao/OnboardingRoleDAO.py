b237db3a254c2e3be918655306ebcdbb24395a5c
Sellular <sellular13@gmail.com>
2022-08-11 04:13:44 +0000

    cleaned up error handling

--- dao/OnboardingRoleDAO.py
+++ dao/OnboardingRoleDAO.py
@@ -23,65 +23,46 @@ __onboardingRoleDeleteByMemberSql = """
 
 
 def getOnboardingRolesByMember(memberID: str):
-    dbConnection = None
-    dbCursor = None
-    try:
-        dbConnection = DBUtils.getDBConnection()
-
-        dbCursor = dbConnection.execute(
-            __onboardingRoleSelectByMemberSql, (memberID,))
-        rows = dbCursor.fetchall()
-
-        if rows:
-            roles = []
-            for row in rows:
-                roles.append(OnboardingRole(
-                    row['member_id'],
-                    row['role_id']
-                ))
-
-            return roles
-    except (Exception) as error:
-        raise error
-    finally:
-        if dbCursor:
-            dbCursor.close()
-        if dbConnection:
-            dbConnection.close()
-
-
-def insertMany(role_list: list):
-    dbConnection = None
-    try:
-        dbConnection = DBUtils.getDBConnection()
-
-        with dbConnection:
-            dbCursor = dbConnection.executemany(
-                __onboardingRoleInsertSql, role_list)
-            dbConnection.commit()
-    except (Exception) as error:
-        raise error
-    finally:
-        if dbCursor:
-            dbCursor.close()
-        if dbConnection:
-            dbConnection.close()
-
-
-def deleteOnboardingRolesByMember(memberID: str):
-    dbConnection = None
-    dbCursor = None
     try:
         dbConnection = DBUtils.getDBConnection()
 
         with dbConnection:
             dbCursor = dbConnection.execute(
+            __onboardingRoleSelectByMemberSql, (memberID,))
+            rows = dbCursor.fetchall()
+
+            if rows:
+                roles = []
+                for row in rows:
+                    roles.append(OnboardingRole(
+                        row['member_id'],
+                        row['role_id']
+                    ))
+
+                return roles
+    except (Exception) as error:
+        raise error
+
+
+def insertMany(role_list: list):
+    try:
+        dbConnection = DBUtils.getDBConnection()
+
+        with dbConnection:
+            dbConnection.executemany(
+                __onboardingRoleInsertSql, role_list)
+            dbConnection.commit()
+    except (Exception) as error:
+        raise error
+
+
+def deleteOnboardingRolesByMember(memberID: str):
+    try:
+        dbConnection = DBUtils.getDBConnection()
+
+        with dbConnection:
+            dbConnection.execute(
                 __onboardingRoleDeleteByMemberSql, (memberID,))
             dbConnection.commit()
     except (Exception) as error:
         raise error
-    finally:
-        if dbCursor:
-            dbCursor.close()
-        if dbConnection:
-            dbConnection.close()
