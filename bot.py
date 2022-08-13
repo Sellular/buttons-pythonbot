ae4edd8e6cad77f8a986529c4693d23bb8b7807e
Sellular <sellular13@gmail.com>
2022-08-13 03:49:50 +0000

    Removed unnecessary comments

--- bot.py
+++ bot.py
@@ -3,13 +3,11 @@ from discord.ext import commands
 
 from utils import SetupUtils, GeneralUtils, DBUtils
 
-# commands.Bot if commands are needed
 class MyClient(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
-        # command_prefix=('!'),
         super().__init__(command_prefix=('!'), intents=intents)
 
     async def on_ready(self):
